d5b09869583b9ba71c31d2fe6be716ce3e0ced88
Banchev <tsvetomir.banchev@sap.com>
2019-07-09 17:53:45 +0300

    for review task 0-3

--- src/main/java/com/saproject/project/ProjectApplication.java
+++ src/main/java/com/saproject/project/ProjectApplication.java
@@ -1,13 +1,27 @@
 package com.saproject.project;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProjectApplication {
+public class ProjectApplication implements ApplicationRunner {
+
+	private static final Logger logger = LogManager.getLogger(ProjectApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectApplication.class, args);
+	}
+	@Override
+	public void run(ApplicationArguments applicationArguments) throws Exception {
+		logger.debug("Debugging log");
+		logger.info("Info log");
+		logger.warn("Hey, This is a warning!");
+		logger.error("Oops! We have an Error. OK");
+		logger.fatal("Damn! Fatal error. Please fix me.");
 	}
 
 }
